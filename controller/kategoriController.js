965ee550104814144932b79eec6e45ea243de05c
MuhSyahrul6 <rulztarito123@gmail.com>
2023-11-23 20:26:07 +0700

    Membuat fungsi Create pada kategoriController

--- controller/kategoriController.js
+++ controller/kategoriController.js
@@ -4,10 +4,27 @@ const kategoriController = {}
     this is auto generate example, you can continue 
 
 */
-kategoriController.index = async(req,res) => {
+kategoriController.index = async (req, res) => {
     res.json({
-        message : "Hello kategoriController"
+        message: "Hello kategoriController"
     })
+}
+
+kategoriController.create = async (req, res) => {
+    const { nama_kategori } = req.body
+    try {
+        const createKategori = await Kategori.create({
+            nama_kategori: nama_kategori
+        })
+        return res.status(201).json({
+            message: 'Data Berhasil Ditambahkan !'
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            message: error
+        })
+    }
 }
 
 module.exports = kategoriController
