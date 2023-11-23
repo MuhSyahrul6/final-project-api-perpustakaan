d6a67e99930f0aa9843311df62d87f787c8d7f2a
MuhSyahrul6 <rulztarito123@gmail.com>
2023-11-23 20:48:20 +0700

    Menambahkan route get

--- router/kategori.js
+++ router/kategori.js
@@ -3,5 +3,6 @@ const kategoriController = require("../controller/kategoriController")
 const routeKategori = express()
 
 routeKategori.post('/', kategoriController.create)
+routeKategori.get('/get', kategoriController.getAll)
 
 module.exports = routeKategori
