1a179edebbed53dc1798c2cf8a02d909825bdddc
MuhSyahrul6 <rulztarito123@gmail.com>
2023-11-23 20:24:42 +0700

    Membuat File baru kategoriController

---
+++ controller/kategoriController.js
@@ -0,0 +1,14 @@
+const kategoriController = {}
+
+/*
+    this is auto generate example, you can continue 
+
+*/
+kategoriController.index = async(req,res) => {
+    res.json({
+        message : "Hello kategoriController"
+    })
+}
+
+module.exports = kategoriController
+
