9050fa70b5949ac506dc34d9de7a089b9173ab69
MuhSyahrul6 <rulztarito123@gmail.com>
2023-11-23 20:36:35 +0700

    Mengimpor Modul controller dan Membuat route Post

--- router/kategori.js
+++ router/kategori.js
@@ -1,5 +1,7 @@
 const express = require("express")
+const kategoriController = require("../controller/kategoriController")
 const routeKategori = express()
 
+routeKategori.post('/', kategoriController.create)
 
 module.exports = routeKategori
