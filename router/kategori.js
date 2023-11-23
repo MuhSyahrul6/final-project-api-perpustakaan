c59906bd631df67b954b264ee584bd17ca0012a1
MuhSyahrul6 <rulztarito123@gmail.com>
2023-11-23 21:10:25 +0700

    Membuat route put untuk Update

--- router/kategori.js
+++ router/kategori.js
@@ -5,5 +5,6 @@ const routeKategori = express()
 routeKategori.post('/', kategoriController.create)
 routeKategori.get('/get', kategoriController.getAll)
 routeKategori.get('/get/:id', kategoriController.getById)
+routeKategori.put('/update/:id', kategoriController.update)
 
 module.exports = routeKategori
