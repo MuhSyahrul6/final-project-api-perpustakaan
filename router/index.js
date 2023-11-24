f1f97908c00bdfef1f4d414067302b6d23d38f95
Muhammad Syahrul Romadhon <135720528+MuhSyahrul6@users.noreply.github.com>
2023-11-24 18:09:16 +0700

    Initial commit

--- router/index.js
+++ router/index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const exampleController = require("../controller/exampleController");
-const routePenulis = require("./penulis");
 const route = express.Router()
 
 route.get('/',exampleController.index)
-route.use('/penulis',routePenulis)
 
 module.exports = route
