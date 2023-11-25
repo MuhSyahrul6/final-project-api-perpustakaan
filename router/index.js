1ee7a9aecb139eb08212f70a55ce966337632427
asahii59 <jreckmaniajaya@gmail.com>
2023-11-25 20:54:37 +0700

    update router

--- router/index.js
+++ router/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const exampleController = require("../controller/exampleController");
+const routePenulis = require("./penulis");
 const route = express.Router()
 
 route.get('/',exampleController.index)
+route.use('/penulis-buku',routePenulis)
 
 module.exports = route
