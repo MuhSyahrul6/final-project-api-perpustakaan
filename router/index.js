f50a3a76a70fd4e59b82ea479a90a98720434cdb
MuhSyahrul6 <rulztarito123@gmail.com>
2023-11-25 21:49:12 +0700

    Update index

--- router/index.js
+++ router/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const exampleController = require("../controller/exampleController");
 const routePenulis = require("./penulis");
+const routeKategori = require("./kategori");
 const route = express.Router()
 
-route.get('/',exampleController.index)
-route.use('/penulis-buku',routePenulis)
+route.get('/', exampleController.index)
+route.use('/kategori-buku', routeKategori)
+route.use('/penulis-buku', routePenulis)
 
 module.exports = route
