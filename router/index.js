3032eccc5a48b63b1d045f744d83d2a4edeb111f
Muhammad Syahrul Romadhon <135720528+MuhSyahrul6@users.noreply.github.com>
2023-11-25 21:21:30 +0700

    Delete router/index.js

--- router/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const exampleController = require("../controller/exampleController");
-//const routeKategori = require("./kategori");
-const route = express.Router()
-
-route.get('/', exampleController.index)
-//route.use('/kategori-buku', routeKategori)
-
-module.exports = route
