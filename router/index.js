6de1fed02a548295e07c438cea79eca8613cbb63
Muhammad Syahrul Romadhon <135720528+MuhSyahrul6@users.noreply.github.com>
2023-11-30 17:25:00 +0700

    Update index.js

--- router/index.js
+++ router/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const exampleController = require("../controller/exampleController");
+const routeAnggota = require("./anggota");
 const route = express.Router()
 
 route.get('/',exampleController.index)
+route.use('/anggota', routeAnggota)
 
 module.exports = route
