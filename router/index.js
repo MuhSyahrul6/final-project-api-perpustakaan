186d093b435577442f11354064787f068c7fec1d
Muhammad Syahrul Romadhon <135720528+MuhSyahrul6@users.noreply.github.com>
2023-12-06 14:18:21 +0700

    Melakukan Update index.js

--- router/index.js
+++ router/index.js
@@ -5,6 +5,7 @@ const routePenulis = require("./penulis");
 const routeBuku = require("./buku");
 const routeAnggota = require("./anggota");
 const routeDenda = require("./denda")
+const routePeminjaman = require("./peminjaman");
 const route = express.Router()
 
 route.get('/', exampleController.index)
@@ -13,5 +14,6 @@ route.use('/penulis-buku', routePenulis)
 route.use('/buku', routeBuku)
 route.use('/anggota', routeAnggota)
 route.use('/denda', routeDenda)
+route.use('/peminjaman', routePeminjaman)
 
 module.exports = route
