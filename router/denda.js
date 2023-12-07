e7b5552392958a0e0b2ec6492dab2bea970267d3
MuhSyahrul6 <rulztarito123@gmail.com>
2023-12-07 17:29:49 +0700

    Membuat autentikasi untuk denda

--- router/denda.js
+++ router/denda.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const dendaController = require("../controller/dendaController")
+const isAdmin = require("../middleware/isAdmin");
+const isUser = require("../middleware/isUser");
 const routeDenda = express.Router()
 
-routeDenda.post('/', dendaController.create)
-routeDenda.get('/get', dendaController.getAll)
-routeDenda.get('/get/:id', dendaController.getById)
-routeDenda.put('/update/:id', dendaController.update)
-routeDenda.delete('/delete/:id', dendaController.delete)
+routeDenda.post('/', isAdmin, dendaController.create)
+routeDenda.get('/get', isAdmin, dendaController.getAll)
+routeDenda.get('/get/:id', isAdmin, dendaController.getById)
+routeDenda.put('/update/:id', isAdmin, dendaController.update)
+routeDenda.delete('/delete/:id', isAdmin, dendaController.delete)
 
 module.exports = routeDenda
