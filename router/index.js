07c9a94c01b2201551872b8f2753b055a0b7d87d
DaffaYuza <muhammaddaffayuza@gmail.com>
2023-12-07 18:12:31 +0700

    menambahkan authVerify pada router index

--- router/index.js
+++ router/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const exampleController = require("../controller/exampleController");
+const authVerify = require("../middleware/authVerify")
+const routeUser = require("./user")
 const routeKategori = require("./kategori");
 const routePenulis = require("./penulis");
 const routeBuku = require("./buku");
@@ -9,11 +11,13 @@ const routePeminjaman = require("./peminjaman");
 const route = express.Router()
 
 route.get('/', exampleController.index)
-route.use('/kategori-buku', routeKategori)
-route.use('/penulis-buku', routePenulis)
-route.use('/buku', routeBuku)
-route.use('/anggota', routeAnggota)
-route.use('/denda', routeDenda)
-route.use('/peminjaman', routePeminjaman)
+route.use('/kategori-buku', (req, res, next) => authVerify(req, res, next), routeKategori)
+route.use('/penulis-buku', (req, res, next) => authVerify(req, res, next), routePenulis)
+route.use('/buku', (req, res, next) => authVerify(req, res, next), routeBuku)
+route.use('/anggota', (req, res, next) => authVerify(req, res, next), routeAnggota)
+route.use('/denda', (req, res, next) => authVerify(req, res, next), routeDenda)
+route.use('/peminjaman', (req, res, next) => authVerify(req, res, next), routePeminjaman)
+
+route.use('/auth', routeUser)
 
 module.exports = route
