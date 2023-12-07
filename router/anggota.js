bf79475bfb01f537a0151f5e7caf1a2ce6c58e82
MuhSyahrul6 <rulztarito123@gmail.com>
2023-12-07 17:28:30 +0700

    Membuat autentikasi untuk anggota

--- router/anggota.js
+++ router/anggota.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const anggotaController = require("../controller/anggotaController")
+const isAdmin = require("../middleware/isAdmin");
+const isUser = require("../middleware/isUser");
 const routeAnggota = express.Router()
 
-routeAnggota.post('/', anggotaController.create)
-routeAnggota.get('/get', anggotaController.getAll)
-routeAnggota.get('/get/:id', anggotaController.getById)
-routeAnggota.put('/update/:id', anggotaController.update)
-routeAnggota.delete('/delete/:id', anggotaController.delete)
+routeAnggota.post('/', isAdmin, anggotaController.create)
+routeAnggota.get('/get', isUser, anggotaController.getAll)
+routeAnggota.get('/get/:id', isUser, anggotaController.getById)
+routeAnggota.put('/update/:id', isAdmin, anggotaController.update)
+routeAnggota.delete('/delete/:id', isAdmin, anggotaController.delete)
 
 module.exports = routeAnggota
